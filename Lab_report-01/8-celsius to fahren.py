05e832fef2ed95ff6cf6a2d5f454e82763955c40
raduan-ahmed <raduanahmed49591@gmail.com>
2024-09-11 23:31:08 +0600

    commit-7

---
+++ Lab_report-01/8-celsius to fahren.py
@@ -0,0 +1,8 @@
+# Input temperature in Celsius
+celsius = float(input("Enter temperature in Celsius: "))
+
+# Convert to Fahrenheit (F = C * 9/5 + 32)
+fahrenheit = celsius * 9/5 + 32
+
+# Display the result
+print(f"{celsius}°C is equal to {fahrenheit:.2f}°F.")
