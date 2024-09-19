4845cc6e806b7299a876353a50531ba1c6660219
raduan-ahmed <raduanahmed49591@gmail.com>
2024-09-19 23:26:46 +0600

    commit-8

---
+++ Lab_report-01/9-display calendar.py
@@ -0,0 +1,8 @@
+import calendar
+
+# Input year and month
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+
+# Display the calendar
+print(calendar.month(year, month))
