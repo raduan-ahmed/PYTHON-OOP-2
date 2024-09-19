9e30f5e0c5a20004f605d61508dfcf7db446fd8f
raduan-ahmed <raduanahmed49591@gmail.com>
2024-09-19 23:29:39 +0600

    commit-9

---
+++ Lab_report-01/10-check pos neg zero.py
@@ -0,0 +1,10 @@
+# Input from the user
+num = float(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
